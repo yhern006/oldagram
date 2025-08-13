ca223d9189aa4ee9c96f76cc6bae03464dbe7990
Yvette Hernandez <yhern006@users.noreply.github.com>
2025-08-13 13:41:01 -0700

    added post img

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const posts = [
     {
         name: "Vincent van Gogh",
@@ -32,5 +33,33 @@ const feedPostsEl = document.getElementById("feed-posts")
 console.log(feedPostsEl)
 
 function loadPost() {
-    
+    const currentPoster = posts[0]
+
+    let posterInfo = document.createElement('div')
+    posterInfo.setAttribute('class', 'postDiv')
+    document.body.appendChild(posterInfo)
+
+    let posterAvatar = document.createElement('img')
+    posterAvatar.src = currentPoster.avatar
+    posterAvatar.setAttribute('class', 'avatar')
+    posterInfo.appendChild(posterAvatar)
+
+    let wordDiv = document.createElement('div')
+    wordDiv.setAttribute('class', 'wordDiv')
+    posterInfo.appendChild(wordDiv)
+
+    let posterName = document.createElement('p')
+    posterName.textContent = currentPoster.name
+    posterName.setAttribute('class', 'bold-text')
+    wordDiv.appendChild(posterName)
+
+    let posterLocation = document.createElement('p')
+    posterLocation.textContent = currentPoster.location
+    wordDiv.appendChild(posterLocation)
+
+    let posterImg = document.createElement('img')
+    posterImg.src = currentPoster.post
+    posterInfo.appendChild(posterImg)
 }
+
+loadPost()
